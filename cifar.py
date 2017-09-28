69ed515c733df58de68ea61880007a5a768e0eb9
weigq <weigq1234@gmail.com>
2017-09-28 05:01:22 -0400

    mdf

--- cifar.py
+++ cifar.py
@@ -1,14 +1,8 @@
-'''
-Training script for CIFAR-10/100
-Copyright (c) Wei YANG, 2017
-'''
+
 from __future__ import print_function
 
-import argparse
 import os
-import shutil
 import time
-import random
 
 import torch
 import torch.nn as nn
@@ -24,91 +18,40 @@ from utils import Bar, Logger, AverageMeter, accuracy, mkdir_p, savefig
 
 from utils.utils import adjust_learning_rate, save_checkpoint
 
+from cifar_opt import TrainOpt
 
-model_names = sorted(name for name in models.__dict__
-    if name.islower() and not name.startswith("__")
-    and callable(models.__dict__[name]))
+#
+# state = {k: v for k, v in opt._get_kwargs()}
+#
+# # Validate dataset
+# assert opt.dataset == 'cifar10' or opt.dataset == 'cifar100', 'Dataset can only be cifar10 or cifar100.'
+#
+# # Use CUDA
+# os.environ['CUDA_VISIBLE_DEVICES'] = opt.gpu_id
+# use_cuda = torch.cuda.is_available()
+#
+# # Random seed
+# if opt.manualSeed is None:
+#     opt.manualSeed = random.randint(1, 10000)
+# random.seed(opt.manualSeed)
+# torch.manual_seed(opt.manualSeed)
+# if use_cuda:
+#     torch.cuda.manual_seed_all(opt.manualSeed)
 
-parser = argparse.ArgumentParser(description='PyTorch CIFAR10/100 Training')
-# Datasets
-parser.add_argument('-d', '--dataset', default='cifar10', type=str)
-parser.add_argument('-j', '--workers', default=4, type=int, metavar='N',
-                    help='number of data loading workers (default: 4)')
-# Optimization options
-parser.add_argument('--epochs', default=300, type=int, metavar='N',
-                    help='number of total epochs to run')
-parser.add_argument('--start-epoch', default=0, type=int, metavar='N',
-                    help='manual epoch number (useful on restarts)')
-parser.add_argument('--train-batch', default=128, type=int, metavar='N',
-                    help='train batchsize')
-parser.add_argument('--test-batch', default=100, type=int, metavar='N',
-                    help='test batchsize')
-parser.add_argument('--lr', '--learning-rate', default=0.1, type=float,
-                    metavar='LR', help='initial learning rate')
-parser.add_argument('--drop', '--dropout', default=0, type=float,
-                    metavar='Dropout', help='Dropout ratio')
-parser.add_argument('--schedule', type=int, nargs='+', default=[150, 225],
-                        help='Decrease learning rate at these epochs.')
-parser.add_argument('--gamma', type=float, default=0.1, help='LR is multiplied by gamma on schedule.')
-parser.add_argument('--momentum', default=0.9, type=float, metavar='M',
-                    help='momentum')
-parser.add_argument('--weight-decay', '--wd', default=5e-4, type=float,
-                    metavar='W', help='weight decay (default: 1e-4)')
-# Checkpoints
-parser.add_argument('-c', '--checkpoint', default='checkpoint', type=str, metavar='PATH',
-                    help='path to save checkpoint (default: checkpoint)')
-parser.add_argument('--resume', default='', type=str, metavar='PATH',
-                    help='path to latest checkpoint (default: none)')
-# Architecture
-parser.add_argument('--arch', '-a', metavar='ARCH', default='resnet20',
-                    choices=model_names,
-                    help='model architecture: ' +
-                        ' | '.join(model_names) +
-                        ' (default: resnet18)')
-parser.add_argument('--depth', type=int, default=29, help='Model depth.')
-parser.add_argument('--cardinality', type=int, default=8, help='Model cardinality (group).')
-parser.add_argument('--widen-factor', type=int, default=4, help='Widen factor. 4 -> 64, 8 -> 128, ...')
-parser.add_argument('--growthRate', type=int, default=12, help='Growth rate for DenseNet.')
-parser.add_argument('--compressionRate', type=int, default=2, help='Compression Rate (theta) for DenseNet.')
-# Miscs
-parser.add_argument('--manualSeed', type=int, help='manual seed')
-parser.add_argument('-e', '--evaluate', dest='evaluate', action='store_true',
-                    help='evaluate model on validation set')
-#Device options
-parser.add_argument('--gpu_id', default='1', type=str,
-                    help='id(s) for CUDA_VISIBLE_DEVICES')
+best_acc = 0
 
-args = parser.parse_args()
-state = {k: v for k, v in args._get_kwargs()}
 
-# Validate dataset
-assert args.dataset == 'cifar10' or args.dataset == 'cifar100', 'Dataset can only be cifar10 or cifar100.'
-
-# Use CUDA
-os.environ['CUDA_VISIBLE_DEVICES'] = args.gpu_id
-use_cuda = torch.cuda.is_available()
-
-# Random seed
-if args.manualSeed is None:
-    args.manualSeed = random.randint(1, 10000)
-random.seed(args.manualSeed)
-torch.manual_seed(args.manualSeed)
-if use_cuda:
-    torch.cuda.manual_seed_all(args.manualSeed)
-
-best_acc = 0  # best test accuracy
-
-def main():
+def main(opt):
     global best_acc
-    start_epoch = args.start_epoch  # start from epoch 0 or last checkpoint epoch
+    start_epoch = opt.start_epoch  # start from epoch 0 or last checkpoint epoch
 
-    if not os.path.isdir(args.checkpoint):
-        mkdir_p(args.checkpoint)
+    if not os.path.isdir(opt.checkpoint):
+        mkdir_p(opt.checkpoint)
 
 
 
     # Data
-    print('==> Preparing dataset %s' % args.dataset)
+    print('==> Preparing dataset %s' % opt.dataset)
     transform_train = transforms.Compose([
         transforms.RandomCrop(32, padding=4),
         transforms.RandomHorizontalFlip(),
@@ -120,7 +63,7 @@ def main():
         transforms.ToTensor(),
         transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
     ])
-    if args.dataset == 'cifar10':
+    if opt.dataset == 'cifar10':
         dataloader = datasets.CIFAR10
         num_classes = 10
     else:
@@ -129,80 +72,80 @@ def main():
 
 
     trainset = dataloader(root='./data', train=True, download=True, transform=transform_train)
-    trainloader = data.DataLoader(trainset, batch_size=args.train_batch, shuffle=True, num_workers=args.workers)
+    trainloader = data.DataLoader(trainset, batch_size=opt.train_batch, shuffle=True, num_workers=opt.workers)
 
     testset = dataloader(root='./data', train=False, download=False, transform=transform_test)
-    testloader = data.DataLoader(testset, batch_size=args.test_batch, shuffle=False, num_workers=args.workers)
+    testloader = data.DataLoader(testset, batch_size=opt.test_batch, shuffle=False, num_workers=opt.workers)
 
     # Model   
-    print("==> creating model '{}'".format(args.arch))
-    if args.arch.startswith('resnext'):
-        model = models.__dict__[args.arch](
-                    cardinality=args.cardinality,
+    print("==> creating model '{}'".format(opt.arch))
+    if opt.arch.startswith('resnext'):
+        model = models.__dict__[opt.arch](
+                    cardinality=opt.cardinality,
                     num_classes=num_classes,
-                    depth=args.depth,
-                    widen_factor=args.widen_factor,
-                    dropRate=args.drop,
+                    depth=opt.depth,
+                    widen_factor=opt.widen_factor,
+                    dropRate=opt.drop,
                 )
-    elif args.arch.startswith('densenet'):
-        model = models.__dict__[args.arch](
+    elif opt.arch.startswith('densenet'):
+        model = models.__dict__[opt.arch](
                     num_classes=num_classes,
-                    depth=args.depth,
-                    growthRate=args.growthRate,
-                    compressionRate=args.compressionRate,
-                    dropRate=args.drop,
+                    depth=opt.depth,
+                    growthRate=opt.growthRate,
+                    compressionRate=opt.compressionRate,
+                    dropRate=opt.drop,
                 )        
-    elif args.arch.startswith('wrn'):
-        model = models.__dict__[args.arch](
+    elif opt.arch.startswith('wrn'):
+        model = models.__dict__[opt.arch](
                     num_classes=num_classes,
-                    depth=args.depth,
-                    widen_factor=args.widen_factor,
-                    dropRate=args.drop,
+                    depth=opt.depth,
+                    widen_factor=opt.widen_factor,
+                    dropRate=opt.drop,
                 )
-    elif args.arch.endswith('resnet'):
-        model = models.__dict__[args.arch](
+    elif opt.arch.endswith('resnet'):
+        model = models.__dict__[opt.arch](
                     num_classes=num_classes,
-                    depth=args.depth,
+                    depth=opt.depth,
                 )
     else:
-        model = models.__dict__[args.arch](num_classes=num_classes)
+        model = models.__dict__[opt.arch](num_classes=num_classes)
 
     model = torch.nn.DataParallel(model).cuda()
     cudnn.benchmark = True
     print('    Total params: %.2fM' % (sum(p.numel() for p in model.parameters())/1000000.0))
 
     criterion = nn.CrossEntropyLoss()
-    optimizer = optim.SGD(model.parameters(), lr=args.lr, momentum=args.momentum, weight_decay=args.weight_decay)
+    optimizer = optim.SGD(model.parameters(), lr=opt.lr, momentum=opt.momentum, weight_decay=opt.weight_decay)
 
     # Resume
-    title = 'cifar-10-' + args.arch
-    if args.resume:
+    title = 'cifar-10-' + opt.arch
+    if opt.resume:
         # Load checkpoint.
         print('==> Resuming from checkpoint..')
-        assert os.path.isfile(args.resume), 'Error: no checkpoint directory found!'
-        args.checkpoint = os.path.dirname(args.resume)
-        checkpoint = torch.load(args.resume)
+        assert os.path.isfile(opt.resume), 'Error: no checkpoint directory found!'
+        opt.checkpoint = os.path.dirname(opt.resume)
+        checkpoint = torch.load(opt.resume)
         best_acc = checkpoint['best_acc']
         start_epoch = checkpoint['epoch']
         model.load_state_dict(checkpoint['state_dict'])
         optimizer.load_state_dict(checkpoint['optimizer'])
-        logger = Logger(os.path.join(args.checkpoint, 'log.txt'), title=title, resume=True)
+        logger = Logger(os.path.join(opt.checkpoint, 'log.txt'), title=title, resume=True)
     else:
-        logger = Logger(os.path.join(args.checkpoint, 'log.txt'), title=title)
+        logger = Logger(os.path.join(opt.checkpoint, 'log.txt'), title=title)
         logger.set_names(['Learning Rate', 'Train Loss', 'Valid Loss', 'Train Acc.', 'Valid Acc.'])
 
 
-    if args.evaluate:
+    if opt.evaluate:
         print('\nEvaluation only')
         test_loss, test_acc = test(testloader, model, criterion, start_epoch, use_cuda)
         print(' Test Loss:  %.8f, Test Acc:  %.2f' % (test_loss, test_acc))
         return
 
     # Train and val
-    for epoch in range(start_epoch, args.epochs):
+    for epoch in range(start_epoch, opt.epochs):
         adjust_learning_rate(optimizer, epoch)
 
-        print('\nEpoch: [%d | %d] LR: %f' % (epoch + 1, args.epochs, state['lr']))
+        print('\nEpoch: [%d | %d] LR: %f' % (epoch + 1, opt.epochs, state['lr']))
 
         train_loss, train_acc = train(trainloader, model, criterion, optimizer, epoch, use_cuda)
         test_loss, test_acc = test(testloader, model, criterion, epoch, use_cuda)
@@ -219,11 +162,11 @@ def main():
                 'acc': test_acc,
                 'best_acc': best_acc,
                 'optimizer' : optimizer.state_dict(),
-            }, is_best, checkpoint=args.checkpoint)
+            }, is_best, checkpoint=opt.checkpoint)
 
     logger.close()
     logger.plot()
-    savefig(os.path.join(args.checkpoint, 'log.eps'))
+    savefig(os.path.join(opt.checkpoint, 'log.eps'))
 
     print('Best acc:')
     print(best_acc)
@@ -335,5 +278,7 @@ def test(testloader, model, criterion, epoch, use_cuda):
     bar.finish()
     return (losses.avg, top1.avg)
 
+
 if __name__ == '__main__':
-    main()
+    options = TrainOpt().parse()
+    main(options)
---
+++ cifar_opt.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import argparse
+
+__all__ = ['TrainOpt']
+
+
+class TrainOpt:
+    def __init__(self):
+        self.parser = argparse.ArgumentParser()
+        self.opt = None
+
+    def _initial(self):
+        self.parser.add_argument('--dataset',        type=str, default='cifar10')
+        self.parser.add_argument('-j', '--workers',  type=int, default=4)
+
+        self.parser.add_argument('--epochs',         type=int, default=300)
+        self.parser.add_argument('--start_epoch',    type=int, default=0)
+        self.parser.add_argument('--train_batch',    type=int, default=128)
+        self.parser.add_argument('--test_batch',     type=int, default=100)
+        self.parser.add_argument('--lr',             type=float, default=0.1)
+        self.parser.add_argument('--drop',           type=float, default=0)
+        self.parser.add_argument('--lr_step',        type=int, default=60)
+        self.parser.add_argument('--gamma',          type=float, default=0.1, help='rate of lr decay')
+        self.parser.add_argument('--momentum',       type=float, default=0.9)
+        self.parser.add_argument('--weight_decay',   type=float, default=5e-4)
+        self.parser.add_argument('--checkpoint',     type=str, default='checkpoint')
+        self.parser.add_argument('--resume',         type=str, default='')
+
+        self.parser.add_argument('--arch',           type=str, default='resnet20')
+        self.parser.add_argument('--depth',          type=int, default=29)
+        self.parser.add_argument('--cardinality',    type=int, default=8, help='Model cardinality (group).')
+        self.parser.add_argument('--widen_factor',   type=int, default=4, help='Widen factor. 4 -> 64, 8 -> 128, ...')
+        self.parser.add_argument('--growthRate',     type=int, default=12, help='Growth rate for DenseNet.')
+        self.parser.add_argument('--compressionRate', type=int, default=2, help='Compression Rate (theta) for DenseNet.')
+
+        self.parser.add_argument('--manualSeed',     type=int, help='manual seed')
+        self.parser.add_argument('--evaluate',       dest='evaluate', action='store_true')
+
+        self.parser.add_argument('--gpu_id',         type=str, default='1')
+
+    def parse(self):
+        self._initial()
+        self.opt = self.parser.parse_args()
+        return self.opt
+
